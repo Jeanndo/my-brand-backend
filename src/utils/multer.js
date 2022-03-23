5190c8fdf3f163a205349a20260827928ab2024d
System Administrator <root@andelarwandas-MacBook-Pro.local>
2022-03-23 20:22:51 +0200

    ft(updated multer ): integration
    updated
    
    wip

--- src/utils/multer.js
+++ src/utils/multer.js
@@ -7,7 +7,7 @@ export const upload = multer({
   storage: multer.diskStorage({}),
   fileFilter: (req, file, cb) => {
     let ext = path.extname(file.originalname)
-    if (ext !== ".jpeg" && ext !== "jpg" && ext !== "png") {
+    if (ext !== ".jpeg" && ext !== ".jpg" && ext !== ".png") {
       cb(new AppError("file Type is not supported", 400), false)
       return
     }
